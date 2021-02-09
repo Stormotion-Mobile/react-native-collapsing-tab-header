87552dd3ee1a95f731e8d1bba13f7cc79fb215f1
Mikhail Peremitko <mikhailperemitko@gmail.com>
2021-02-09 19:36:14 +0200

    feat: handle variable header height

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,11 +1,10 @@
 import {
   createMaterialTopTabNavigator,
-  MaterialTopTabBar,
-  MaterialTopTabBarProps,
+
+  MaterialTopTabBarProps
 } from "@react-navigation/material-top-tabs";
 import React, { FC, useRef, useState } from "react";
-import { FlatList, FlatListProps } from "react-native";
-import { Animated, StyleProp, View, ViewStyle } from "react-native";
+import { Animated, FlatList, FlatListProps, StyleProp, Text, View, ViewProps, ViewStyle } from "react-native";
 import TabBar from "../components/TabBar";
 import useScrollSync from "../hooks/useScrollSync";
 import useScrollValue from "../hooks/useScrollValue";
@@ -13,7 +12,6 @@ import { Actor } from "../types/Actor";
 import { ScrollPair } from "../types/ScrollPair";
 import Actors from "./Actors";
 
-export const HEADER_HEIGHT = 150;
 export const TAB_BAR = 48;
 
 const Tab = createMaterialTopTabNavigator();
@@ -22,13 +20,17 @@ const Profile: FC = () => {
   const firstListRef = useRef<FlatList>(null);
   const secondListRef = useRef<FlatList>(null);
 
+  const [headerHeight, setHeaderHeight] = useState(0);
   const [tabIndex, setTabIndex] = useState(0);
+
+  const rendered = headerHeight !== 0;
 
   const [
     firstListScrollValue,
     firstListPosition,
     handleFirstListScroll,
   ] = useScrollValue(0);
+
   const [
     secondListScrollValue,
     secondListPosition,
@@ -40,7 +42,7 @@ const Profile: FC = () => {
     { list: secondListRef, position: secondListPosition },
   ];
 
-  const { sync } = useScrollSync(scrollPairs, HEADER_HEIGHT);
+  const { sync } = useScrollSync(scrollPairs, headerHeight);
 
   const currentScrollValue = Animated.add(
     Animated.multiply(firstListScrollValue, tabIndex === 0 ? 1 : 0),
@@ -48,13 +50,13 @@ const Profile: FC = () => {
   );
 
   const translateY = Animated.multiply(-1, currentScrollValue).interpolate({
-    inputRange: [-HEADER_HEIGHT, 0],
-    outputRange: [-HEADER_HEIGHT, 0],
+    inputRange: [-headerHeight, 0],
+    outputRange: [-headerHeight, 0],
     extrapolateLeft: "clamp",
   });
 
   const contentContainerStyle: StyleProp<ViewStyle> = {
-    paddingTop: HEADER_HEIGHT + TAB_BAR,
+    paddingTop: rendered ? headerHeight + TAB_BAR : 0,
   };
 
   const sharedProps: Partial<FlatListProps<Actor>> = {
@@ -81,36 +83,54 @@ const Profile: FC = () => {
 
   const renderTabBar = (props: MaterialTopTabBarProps) => (
     <Animated.View
-      style={{
-        position: "absolute",
-        top: HEADER_HEIGHT,
-        left: 0,
-        right: 0,
-        zIndex: 1,
-        transform: [{ translateY }],
-      }}
+      style={
+        rendered
+          ? {
+              position: "absolute",
+              top: headerHeight,
+              left: 0,
+              right: 0,
+              zIndex: 1,
+              transform: [{ translateY }],
+            }
+          : undefined
+      }
     >
       <TabBar onIndexChange={setTabIndex} {...props} />
     </Animated.View>
   );
 
+  const handleHeaderLayout: NonNullable<ViewProps["onLayout"]> = (event) =>
+    setHeaderHeight(event.nativeEvent.layout.height);
+
   return (
     <View style={{ flex: 1 }}>
+      <Animated.View
+        onLayout={handleHeaderLayout}
+        style={
+          rendered
+            ? {
+                top: 0,
+                left: 0,
+                right: 0,
+                backgroundColor: "blue",
+                position: "absolute",
+                transform: [{ translateY }],
+                zIndex: 1,
+              }
+            : { backgroundColor: "blue" }
+        }
+      >
+        <Text style={{ fontSize: 32 }}>
+          HEY HELLO HEY HELLOHEY HELLOHEY HELLOHEY HELLOHEY HELLOHEY HELLOHEY
+          HELLOHEY HELLOHEY HELLOHEY HELLOHEY HELLOHEY HELLOHEY HELLOHEY
+          HELLOHEY HELLO
+        </Text>
+      </Animated.View>
       <Tab.Navigator tabBar={renderTabBar}>
         <Tab.Screen name="Actors 1">{renderFirstList}</Tab.Screen>
         <Tab.Screen name="Actors 2">{renderSecondList}</Tab.Screen>
       </Tab.Navigator>
-      <Animated.View
-        style={{
-          top: 0,
-          left: 0,
-          right: 0,
-          backgroundColor: "blue",
-          position: "absolute",
-          height: HEADER_HEIGHT,
-          transform: [{ translateY }],
-        }}
-      />
     </View>
   );
 };
